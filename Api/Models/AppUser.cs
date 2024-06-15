0c65bc0087c5c5e7152dd0b278616bd568b21aa1
PatricioOsorio <patriciomiguel_12@hotmail.com>
2024-06-14 18:15:07 -0600

    ✨ feat: add model

---
+++ Api/Models/AppUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Api.Models
+{
+  public class AppUser : IdentityUser
+  {
+        public string Nombres { get; set; }
+        public string ApellidoPaterno { get; set; }
+        public string ApellidoMaterno { get; set; }
+    }
+}
